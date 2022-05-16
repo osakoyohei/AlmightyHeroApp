379e9c9205ac5db4da60f3540306dbfcfd9b43dc
大迫洋平 <osakoyohei@OsakoYoheinoMacBook-Air.local>
2022-05-16 09:16:44 +0900

    日付入力設定の追加

--- AlmightyHeroApp/Controller/EditorViewController.swift
+++ AlmightyHeroApp/Controller/EditorViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import SwiftUI
 
 class EditorViewController: UIViewController {
+    @IBOutlet weak var inputDateTextField: UITextField!
     @IBOutlet weak var inputPushUpTextField: UITextField!
     @IBOutlet weak var inputSitUpTextField: UITextField!
     @IBOutlet weak var inputSquatTextField: UITextField!
@@ -16,6 +17,7 @@ class EditorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDateTextField()
         configureTextField()
     }
 
@@ -27,20 +29,55 @@ class EditorViewController: UIViewController {
         self.view.endEditing(true)
     }
 
-    func configureTextField() {
+    var toolBar: UIToolbar {
         // キーボードに閉じるボタンを作成
         let toolBarRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35)
         let toolBar = UIToolbar(frame: toolBarRect)
         let space = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
         let done = UIBarButtonItem(title: "閉じる", style: .done, target: self, action: #selector(didTapDone))
         toolBar.items = [space, done]
-        inputPushUpTextField.inputAccessoryView = toolBar
-        inputSitUpTextField.inputAccessoryView = toolBar
-        inputSquatTextField.inputAccessoryView = toolBar
-        inputRunningTextField.inputAccessoryView = toolBar
         // 他の場所をタップした時にキーボードが閉じるようにする
         let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGR.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tapGR)
+        return toolBar
+    }
+
+    @objc func didChangeDate(picker: UIDatePicker) {
+        inputDateTextField.text = dateFormatter.string(from: picker.date)
+    }
+
+    var datePicker: UIDatePicker {
+        let datePicker: UIDatePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.timeZone = .current
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ja-JP")
+        datePicker.date = Date()
+        datePicker.addTarget(self, action: #selector(didChangeDate), for: .valueChanged)
+        return datePicker
+    }
+
+    var dateFormatter: DateFormatter {
+        let dateFormatt = DateFormatter()
+        dateFormatt.dateStyle = .long
+        dateFormatt.timeZone = .current
+        dateFormatt.locale = Locale(identifier: "ja-JP")
+        return dateFormatt
+    }
+
+    // 日付テキストフィールドの設定
+    func configureDateTextField() {
+        inputDateTextField.inputView = datePicker
+        inputDateTextField.inputAccessoryView = toolBar
+        inputDateTextField.text = dateFormatter.string(from: Date())
+    }
+
+    // 腕立て伏せ、上体起こし、スクワット、ランニングのテキストフィールドの設定
+    func configureTextField() {
+        inputPushUpTextField.inputAccessoryView = toolBar
+        inputSitUpTextField.inputAccessoryView = toolBar
+        inputSquatTextField.inputAccessoryView = toolBar
+        inputRunningTextField.inputAccessoryView = toolBar
     }
 }
